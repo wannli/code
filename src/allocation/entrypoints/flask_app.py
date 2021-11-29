c68672f1a454afdcacb203e74805178fbdb31fd3
Wannes Lint <contact@wanneslint.net>
2021-11-28 19:53:32 -0500

    fastapi: router update

--- src/allocation/entrypoints/flask_app.py
+++ src/allocation/entrypoints/flask_app.py
@@ -1,15 +1,20 @@
 from datetime import datetime
-from flask import Flask, jsonify, request
+
+# from flask import Flask, jsonify, request
+from fastapi import FastAPI
+from starlette.requests import Request
 from allocation.domain import commands
 from allocation.service_layer.handlers import InvalidSku
 from allocation import bootstrap, views
 
-app = Flask(__name__)
+# app = Flask(__name__)
+app = FastAPI()
 bus = bootstrap.bootstrap()
 
 
-@app.route("/add_batch", methods=["POST"])
-def add_batch():
+# @app.route("/add_batch", methods=["POST"])
+@app.post("/add_batch")
+def add_batch(request: Request):
     eta = request.json["eta"]
     if eta is not None:
         eta = datetime.fromisoformat(eta).date()
@@ -20,8 +25,9 @@ def add_batch():
     return "OK", 201
 
 
-@app.route("/allocate", methods=["POST"])
-def allocate_endpoint():
+# @app.route("/allocate", methods=["POST"])
+@app.post("/allocate")
+def allocate_endpoint(request: Request):
     try:
         cmd = commands.Allocate(
             request.json["orderid"], request.json["sku"], request.json["qty"]
@@ -33,8 +39,9 @@ def allocate_endpoint():
     return "OK", 202
 
 
-@app.route("/allocations/<orderid>", methods=["GET"])
-def allocations_view_endpoint(orderid):
+# @app.route("/allocations/<orderid>", methods=["GET"])
+@app.get("/allocations/{orderid}")
+def allocations_view_endpoint(orderid: int, request: Request):
     result = views.allocations(orderid, bus.uow)
     if not result:
         return "not found", 404
