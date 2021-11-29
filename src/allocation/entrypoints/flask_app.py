412765013c3f87b3c969c00765ca1d1546a441c4
Wannes Lint <contact@wanneslint.net>
2021-11-28 20:50:00 -0500

    fastapi: parameters update

--- src/allocation/entrypoints/flask_app.py
+++ src/allocation/entrypoints/flask_app.py
@@ -14,34 +14,28 @@ bus = bootstrap.bootstrap()
 
 # @app.route("/add_batch", methods=["POST"])
 @app.post("/add_batch")
-def add_batch(request: Request):
-    eta = request.json["eta"]
+def add_batch(ref: str, sku: str, qty: int, eta: str):
     if eta is not None:
         eta = datetime.fromisoformat(eta).date()
-    cmd = commands.CreateBatch(
-        request.json["ref"], request.json["sku"], request.json["qty"], eta
-    )
+    cmd = commands.CreateBatch(ref, sku, qty, eta)
     bus.handle(cmd)
     return "OK", 201
 
 
 # @app.route("/allocate", methods=["POST"])
 @app.post("/allocate")
-def allocate_endpoint(request: Request):
+def allocate_endpoint(orderid: str, sku: str, qty: int):
     try:
-        cmd = commands.Allocate(
-            request.json["orderid"], request.json["sku"], request.json["qty"]
-        )
+        cmd = commands.Allocate(orderid, sku, qty)
         bus.handle(cmd)
     except InvalidSku as e:
         return {"message": str(e)}, 400
-
     return "OK", 202
 
 
 # @app.route("/allocations/<orderid>", methods=["GET"])
 @app.get("/allocations/{orderid}")
-def allocations_view_endpoint(orderid: int, request: Request):
+def allocations_view_endpoint(orderid: str):
     result = views.allocations(orderid, bus.uow)
     if not result:
         return "not found", 404
